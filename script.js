a893434de1c5110f018410fe0cb806298e980241
DorlacDev <dorlacdev@gmail.com>
2020-06-20 16:12:25 -0400

    Fix button to reflect new design

--- script.js
+++ script.js
@@ -3,18 +3,18 @@ function login() {
     let legend = document.querySelector('legend');
     let submit = document.querySelector('#submit');
     let button = document.querySelector('button');
-    if (legend.textContent == 'Login') {
+    if (legend.textContent == 'Log in to Transitworks') {
         form.setAttribute('action', 'createaccount.py');
         form.setAttribute('method', 'post');
-        legend.textContent = 'Create Account';
-        submit.setAttribute('value', 'Create Account');
-        button.textContent = 'login';
+        legend.textContent = 'Create your account';
+        submit.setAttribute('value', 'Sign up');
+        button.textContent = 'Log in to Transitworks';
     } else {
         form.setAttribute('action', 'login.py');
         form.setAttribute('method', 'get');
-        legend.textContent = 'Login';
-        submit.setAttribute('value', 'Login');
-        button.textContent = 'create account';
+        legend.textContent = 'Log in to Transitworks';
+        submit.setAttribute('value', 'Log in');
+        button.textContent = 'Sign up for Transitworks';
     }
 }
 
