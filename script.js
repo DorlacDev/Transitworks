bc0ef2af56d8db3923ecad9f99dfc60f06807942
DorlacDev <dorlacdev@gmail.com>
2020-06-20 18:19:21 -0400

    Use vscode formatter

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
 function login() {
-    let form = document.querySelector('form');
-    let legend = document.querySelector('legend');
-    let submit = document.querySelector('#submit');
-    let button = document.querySelector('button');
-    if (legend.textContent == 'Log in to Transitworks') {
-        form.setAttribute('action', 'createaccount.py');
-        form.setAttribute('method', 'post');
-        legend.textContent = 'Create your account';
-        submit.setAttribute('value', 'Sign up');
-        button.textContent = 'Log in to Transitworks';
-    } else {
-        form.setAttribute('action', 'login.py');
-        form.setAttribute('method', 'get');
-        legend.textContent = 'Log in to Transitworks';
-        submit.setAttribute('value', 'Log in');
-        button.textContent = 'Sign up for Transitworks';
-    }
+  let form = document.querySelector("form");
+  let legend = document.querySelector("legend");
+  let submit = document.querySelector("#submit");
+  let button = document.querySelector("button");
+  if (legend.textContent == "Log in to Transitworks") {
+    form.setAttribute("action", "createaccount.py");
+    form.setAttribute("method", "post");
+    legend.textContent = "Create your account";
+    submit.setAttribute("value", "Sign up");
+    button.textContent = "Log in to Transitworks";
+  } else {
+    form.setAttribute("action", "login.py");
+    form.setAttribute("method", "get");
+    legend.textContent = "Log in to Transitworks";
+    submit.setAttribute("value", "Log in");
+    button.textContent = "Sign up for Transitworks";
+  }
 }
 
-var modify = (function() {
-    let button = document.querySelector('button');
-    button.addEventListener('click', login);
-}());
+var modify = (function () {
+  let button = document.querySelector("button");
+  button.addEventListener("click", login);
+})();
